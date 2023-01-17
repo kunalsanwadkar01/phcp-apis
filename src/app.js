92cea1901821280880b9be1fa2f45286c1a8cb6b
kunalsanwadkar01 <kunalsanwadkar@gmail.com>
2023-01-17 14:21:04 +0530

    port change

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ require('./db/connection');
 const Doctor = require('./models/doctors'); 
 
 const app = express();
-const port= process.env.PORT || 3000
+const port= process.env.PORT || 8000
 
 app.use(express.json());
 
